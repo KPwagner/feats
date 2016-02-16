b191ee30cea4ae97c780e1ed1e2c82d837a63f2e
Kyle Wagner <kyle.p.wagner@gmail.com>
2016-02-16 01:13:21 -0600

    Add JSON template

--- scraper.py
+++ scraper.py
@@ -9,8 +9,21 @@ def get_page(url):
 	# print 'Response Status Code for ' + str(url) + ': ' + str(response.getcode())
 	return page
 
+
+feat_template = """
+	"%feat%" : {
+		"name": "%name%",
+		"featPrerequisites": %featPrerequisites%,
+		"nonFeatPrerequisites": %nonFeatPrerequisites%,
+		"buildsInto": %buildsInto%,
+		"benefitsShort": "%buildsInto%"
+	},
+"""
+feats_json = "var feats = {" + feat_template + "}"
+
+
 file = open("feats.txt", 'w')
-file.write("this is only a test.")
+file.write(feats_json)
 file.close()
 
 core_feats_url = "http://paizo.com/pathfinderRPG/prd/coreRulebook/feats.html"
