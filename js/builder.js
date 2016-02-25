25593cb0afdf6a9a5bbce4a2b6058ade418f194b
Kyle Wagner <kyle.p.wagner@gmail.com>
2016-02-25 15:02:48 -0600

    Add builds-into divs

--- js/builder.js
+++ js/builder.js
@@ -1,5 +1,6 @@
 $(function(){
 	var featDiv = "<div class='feat' id='%id%'><h2>%name%</h2><p>%nonFeatPrerequisites%</p><p>%benefitsShort%</p></div>"
+	var buildIntoFeatDiv = "<div class='build-into-feat' id='%id%'><h2>%name%</h2><p>%nonFeatPrerequisites%</p><p>%benefitsShort%</p></div>"
 	var buildLink = '<a href="">See Build Path</a>'
 
 
@@ -25,6 +26,31 @@ $(function(){
 
 			if (feats[feat]["buildsInto"][0]) {
 				$("#"+id).append(buildLink);
+			}
+			for (buildIntoFeat in feats[feat]["buildsInto"]){
+				var buildIntoId, name, nonFeatPrerequisites, benefitsShort;
+
+				buildIntoId = feats[feat]["buildsInto"][buildIntoFeat];
+				name = feats[buildIntoId]["name"];
+				nonFeatPrerequisites = feats[buildIntoId]["nonFeatPrerequisites"];
+				if (nonFeatPrerequisites == false) {
+					nonFeatPrerequisites = "No Prerequisites";
+				}
+				benefitsShort = feats[buildIntoId]["benefitsShort"];
+
+				formattedFeat = buildIntoFeatDiv
+				.replace("%id%", buildIntoId)
+				.replace("%name%", name)
+				.replace("%nonFeatPrerequisites%", nonFeatPrerequisites)
+				.replace("%benefitsShort%", benefitsShort);
+
+				$("#" + id).append(formattedFeat);
+				if (feats[buildIntoId]["buildsInto"][0]) {
+					$("#"+buildIntoId).append(buildLink);
+				}
+				for (i in feats[buildIntoId]["buildsInto"]){
+					console.log(feats[buildIntoId]["buildsInto"]);
+				}
 			}
 		}
 	}
